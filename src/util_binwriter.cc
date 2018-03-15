97fcd16a85b7ad155baa35651a1a7a68cf92bc9f
chekwob <chek@wobbyworks.com>
2018-03-15 22:49:59 +0000

    [prep] unit tests for util::binwriter; still missing write_xbits

--- src/util_binwriter.cc
+++ src/util_binwriter.cc
@@ -134,5 +134,168 @@ void binwriter::write_sbits(int bits, int64_t value)
     write_ubits(bits, u_value);
 }
 
+#if FEATURE_INTERNAL_TEST
+namespace {
+
+TEST(util_binwriter, U8Write)
+{
+    const blob data = { 0, 1, 0x7F, 0x80, 0xFF };
+    binwriter w;
+    w.begin();
+    w.write_u8(0);
+    w.write_u8(1);
+    w.write_u8(0x7F);
+    w.write_u8(0x80);
+    w.write_u8(0xFF);
+    EXPECT_EQ(w.end(), data);
+}
+
+TEST(util_binwriter, U16Write)
+{
+    const blob data = {
+        0, 0,
+        1, 0,
+        0xFF, 0x7F,
+        0x00, 0x80,
+        0xFF, 0xFF
+    };
+    binwriter w;
+    w.begin();
+    w.write_u16(0);
+    w.write_u16(1);
+    w.write_u16(0x7FFF);
+    w.write_u16(0x8000);
+    w.write_u16(0xFFFF);
+    EXPECT_EQ(w.end(), data);
+}
+
+TEST(util_binwriter, U32Write)
+{
+    const blob data = {
+        0, 0, 0, 0,
+        1, 0, 0, 0,
+        0xFF, 0xFF, 0xFF, 0x7F,
+        0x00, 0x00, 0x00, 0x80,
+        0xFF, 0xFF, 0xFF, 0xFF
+    };
+    binwriter w;
+    w.begin();
+    w.write_u32(0);
+    w.write_u32(1);
+    w.write_u32(0x7FFFFFFFLL);
+    w.write_u32(0x80000000LL);
+    w.write_u32(0xFFFFFFFFLL);
+    EXPECT_EQ(w.end(), data);
+}
+
+TEST(util_binwriter, UBitsWrite)
+{
+    const blob data = { 0b01000010, 0b10111010, 0b10101010, 0b10101010, 0b11101010 };
+    binwriter w;
+    w.begin();
+    w.write_ubits( 0, 0);
+    w.write_ubits( 1, 0);
+    w.write_ubits( 1, 1);
+    w.write_ubits( 4, 0);
+    w.write_ubits( 4, 0b1001); // crosses a byte boundary
+    w.write_ubits( 5, 0b01110);
+    w.write_ubits(23, 0b10101010101010101010101);
+    w.write_ubits( 2, 0b11);
+    EXPECT_EQ(w.end(), data);
+}
+
+TEST(util_binwriter, S8Write)
+{
+    const blob data = { 0, 1, 0x7F, 0x80, 0xFF };
+    binwriter w;
+    w.begin();
+    w.write_s8(0);
+    w.write_s8(1);
+    w.write_s8(0x7F);
+    w.write_s8(-0x80);
+    w.write_s8(-1);
+    EXPECT_EQ(w.end(), data);
+}
+
+TEST(util_binwriter, S16Write)
+{
+    const blob data = {
+        0, 0,
+        1, 0,
+        0xFF, 0x7F,
+        0x00, 0x80,
+        0xFF, 0xFF
+    };
+    binwriter w;
+    w.begin();
+    w.write_s16(0);
+    w.write_s16(1);
+    w.write_s16(0x7FFF);
+    w.write_s16(-0x8000);
+    w.write_s16(-1);
+    EXPECT_EQ(w.end(), data);
+}
+
+TEST(util_binwriter, S32Write)
+{
+    const blob data = {
+        0, 0, 0, 0,
+        1, 0, 0, 0,
+        0xFF, 0xFF, 0xFF, 0x7F,
+        0x00, 0x00, 0x00, 0x80,
+        0xFF, 0xFF, 0xFF, 0xFF
+    };
+    binwriter w;
+    w.begin();
+    w.write_s32(0);
+    w.write_s32(1);
+    w.write_s32(0x7FFFFFFFLL);
+    w.write_s32(-0x80000000LL);
+    w.write_s32(-1LL);
+    EXPECT_EQ(w.end(), data);
+}
+
+TEST(util_binwriter, SBitsWrite)
+{
+    const blob data = { 0b01000010, 0b10111010, 0b10101010, 0b10101010, 0b11101010 };
+    binwriter w;
+    w.begin();
+    w.write_sbits( 0, 0);
+    w.write_sbits( 1, 0);
+    w.write_sbits( 1, -1);
+    w.write_sbits( 4, 0);
+    w.write_sbits( 4, -0b0111); // crosses a byte boundary
+    w.write_sbits( 5, 0b01110);
+    w.write_sbits(23, -0b01010101010101010101011);
+    w.write_sbits( 2, -0b01);
+    EXPECT_EQ(w.end(), data);
+}
+
+TEST(util_binwriter, PartialBitError)
+{
+    binwriter w;
+    w.begin();
+    w.write_ubits(1, 0);
+    EXPECT_THROW(w.write_u8(0), std::logic_error);
+}
+
+TEST(util_binwriter, DoubleBeginError)
+{
+    binwriter w;
+    w.begin();
+    EXPECT_THROW(w.begin(), std::logic_error);
+}
+
+TEST(util_binwriter, DoubleEndError)
+{
+    binwriter w;
+    w.begin();
+    w.end();
+    EXPECT_THROW(w.end(), std::logic_error);
+}
+
+}
+#endif
+
 }
 }
