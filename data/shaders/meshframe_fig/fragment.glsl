08c92056dc1f3149ce4e013324456db97e4352c1
chekwob <chek@wobbyworks.com>
2020-03-31 23:14:52 +0000

    Reformat tpage textures and support texel value 0

--- data/shaders/meshframe_fig/fragment.glsl
+++ data/shaders/meshframe_fig/fragment.glsl
@@ -18,7 +18,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
-uniform isampler2D u_Textures[8];
+uniform usampler2D u_Textures[8];
 
 smooth in vec3 v_Color;
 smooth in vec2 v_Texcoord;
@@ -33,52 +33,43 @@ int bitfieldExtract(int a, int b, int c)
   return (a >> b) & mask;
 }
 
-ivec4 texelFetchFromPage(ivec2 coords)
+vec3 unpackBGR555(int value)
+{
+    return vec3(
+        bitfieldExtract(value, 0, 5) / 32.0,
+        bitfieldExtract(value, 5, 5) / 32.0,
+        bitfieldExtract(value, 10, 5) / 32.0
+    );
+}
+
+int sample16(ivec2 uv)
 {
     switch (v_TexpageIndex) {
-    case 0: return texelFetch(u_Textures[0], coords, 0);
-    case 1: return texelFetch(u_Textures[1], coords, 0);
-    case 2: return texelFetch(u_Textures[2], coords, 0);
-    case 3: return texelFetch(u_Textures[3], coords, 0);
-    case 4: return texelFetch(u_Textures[4], coords, 0);
-    case 5: return texelFetch(u_Textures[5], coords, 0);
-    case 6: return texelFetch(u_Textures[6], coords, 0);
-    case 7: return texelFetch(u_Textures[7], coords, 0);
+    case 0: return int(texelFetch(u_Textures[0], uv, 0).r);
+    case 1: return int(texelFetch(u_Textures[1], uv, 0).r);
+    case 2: return int(texelFetch(u_Textures[2], uv, 0).r);
+    case 3: return int(texelFetch(u_Textures[3], uv, 0).r);
+    case 4: return int(texelFetch(u_Textures[4], uv, 0).r);
+    case 5: return int(texelFetch(u_Textures[5], uv, 0).r);
+    case 6: return int(texelFetch(u_Textures[6], uv, 0).r);
+    case 7: return int(texelFetch(u_Textures[7], uv, 0).r);
     }
 }
 
-vec4 sample16(ivec2 uv)
+int sample8(ivec2 uv, int clut_y)
 {
-  ivec4 texel = texelFetchFromPage(ivec2(uv.x / 2, uv.y));
-  int pixel = ((uv.x & 1) == 1) ? (texel.a << 8) + texel.b : (texel.g << 8) + texel.r;
+    int texel = sample16(ivec2(uv.x / 2, uv.y));
+    int cindex = bitfieldExtract(texel, (uv.x & 1) * 8, 8);
 
-  return vec4(
-    ((pixel >> 0) & 0x1F) / 32.0f,
-    ((pixel >> 5) & 0x1F) / 32.0f,
-    ((pixel >> 10) & 0x1F) / 32.0f,
-    ((pixel >> 15) & 1) / 1.0f
-  );
+    return sample16(ivec2(cindex, clut_y));
 }
 
-vec4 sample8(ivec2 uv, int clut_y) 
+int sample4(ivec2 uv, int clut_y, int clut_x)
 {
-  ivec4 texel = texelFetchFromPage(ivec2(uv.x / 4, uv.y));
-  int cindex = ((uv.x & 2) == 2) ? ((uv.x & 1) == 1) ? texel.a : texel.b 
-                                 : ((uv.x & 1) == 1) ? texel.g : texel.r;
-  
-  return sample16(ivec2(cindex, clut_y));
-}
+    int texel = sample16(ivec2(uv.x / 4, uv.y));
+    int cindex = bitfieldExtract(texel, (uv.x & 3) * 4, 4);
 
-vec4 sample4(ivec2 uv, int clut_y, int clut_x)
-{
-  ivec4 texel = texelFetchFromPage(ivec2(uv.x / 8, uv.y));
-  int cindex = ((uv.x & 4) == 4) ? 
-        ((uv.x & 2) == 2) ? ((uv.x & 1) != 1) ? texel.a & 0xF : (texel.a >> 4) & 0xF
-                          : ((uv.x & 1) != 1) ? texel.b & 0xF : (texel.b >> 4) & 0xF
-      : ((uv.x & 2) == 2) ? ((uv.x & 1) != 1) ? texel.g & 0xF : (texel.g >> 4) & 0xF
-                          : ((uv.x & 1) != 1) ? texel.r & 0xF : (texel.r >> 4) & 0xF;
-
-  return sample16(ivec2(cindex + clut_x * 16, clut_y));
+    return sample16(ivec2(cindex + clut_x * 16, clut_y));
 }
 
 void main()
@@ -86,19 +77,25 @@ void main()
     f_Color = vec4(v_Color, 1.0);
 
     if (v_Texinfo != 0) {
-        f_Color *= 2.0;
-
         int color_mode = bitfieldExtract(v_Texinfo, 0, 2);
         int clut_x = bitfieldExtract(v_Texinfo, 19, 4);
         int clut_y = bitfieldExtract(v_Texinfo, 23, 7);
         int page_width = 128 * (2 << (2 - color_mode));
         ivec2 texcoord = ivec2(v_Texcoord.x * page_width, v_Texcoord.y * 128);
+
+        int texcolor;
         if (color_mode == 0) {
-            f_Color *= sample4(texcoord, clut_y, clut_x);
+            texcolor = sample4(texcoord, clut_y, clut_x);
         } else if (color_mode == 1) {
-            f_Color *= sample8(texcoord, clut_y);
+            texcolor = sample8(texcoord, clut_y);
         } else {
-            f_Color *= sample16(texcoord);
+            texcolor = sample16(texcoord);
+        }
+
+        if (texcolor == 0) {
+            discard;
+        } else {
+            f_Color.rgb *= 2.0 * unpackBGR555(texcolor);
         }
     }
 }
--- src/render_meshframe_fig.cc
+++ src/render_meshframe_fig.cc
@@ -95,7 +95,7 @@ void meshframe_fig::draw(const scene::env &e)
         if (!texture->m_texture.ok) {
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8UI, 128, 128, 0, GL_RGBA_INTEGER, GL_UNSIGNED_BYTE, texture->get_texels().data());
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_R16UI, 256, 128, 0, GL_RED_INTEGER, GL_UNSIGNED_SHORT, texture->get_texels().data());
             texture->m_texture.ok = true;
         }
     }
